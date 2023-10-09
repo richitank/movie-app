5001aabdd5d68c46d5070e56660381163cc7d91a
Richi Tank <richitank4@gmail.com>
2023-10-08 20:34:11 -0400

    Added and updated css for movies page

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -7,7 +7,7 @@ import { MoviesList } from './moviesList.model';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+  styleUrls: ['./movies.component.scss']
 })
 
 export class MoviesComponent implements OnInit {
